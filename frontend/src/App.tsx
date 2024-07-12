f3790d20adfd999e4b388ea3983079819a74e511
sherry wen <rikka9816@gmail.com>
2024-07-12 20:17:50 +1200

    mod: change import format for CI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Homepage } from "./pages/HomePage/HomePage.js";
+import { Homepage } from "./pages/HomePage/HomePage";
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
-import ErrorPage from "./ErrorPage.js";
-import CraftPageContainer from "./pages/CraftPage/CraftPageContainer.js";
+import { store } from "./store/store";
+import ErrorPage from "./ErrorPage";
+import CraftPageContainer from "./pages/CraftPage/CraftPageContainer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
