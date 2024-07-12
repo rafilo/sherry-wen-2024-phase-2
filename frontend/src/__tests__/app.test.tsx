4f5ee535b9158507a8df9120e046091485efbee2
sherry wen <rikka9816@gmail.com>
2024-07-12 21:36:41 +1200

    add: add alias for './src' path

--- frontend/src/__tests__/app.test.tsx
+++ frontend/src/__tests__/app.test.tsx
@@ -1,7 +1,6 @@
 // todo: test installation of app.jsx
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 // npm test -- app.test.jsx
 describe("homepage test", () => {
