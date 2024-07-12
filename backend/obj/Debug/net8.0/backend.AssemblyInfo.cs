1067952773252c9be5fa9400948180cb3147f729
sherry wen <rikka9816@gmail.com>
2024-07-12 15:36:48 +1200

    add: add CI for frontend

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb593051f046f7e128d417fbc7d7b2671bbe7f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29aed5d3a313004bb45cd9dff7202d214f2dd1c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
