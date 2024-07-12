5e64282995d97b8d074653d03662e108af6c8455
sherry wen <rikka9816@gmail.com>
2024-07-12 21:38:54 +1200

    add: add alias for './src' path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Homepage from "@/pages/HomePage/HomePage";
+import Homepage from "./pages/HomePage/HomePage";
 import { Provider } from "react-redux";
-import { store } from "@/store/store";
-import ErrorPage from "@/ErrorPage";
-import CraftPageContainer from "@/pages/CraftPage/CraftPageContainer";
+import { store } from "./store/store";
+import ErrorPage from "./ErrorPage";
+import CraftPageContainer from "./pages/CraftPage/CraftPageContainer";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'src': path.resolve(__dirname, './src'),
     },
   },
   define: {
