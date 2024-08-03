761bfca2c1418862a67341b43b62d959775f7700
sherry wen <rikka9816@gmail.com>
2024-08-03 22:42:06 +1200

    feat: remove production environment setting

--- backend/Program.cs
+++ backend/Program.cs
@@ -44,18 +44,6 @@ if (app.Environment.IsDevelopment())
     app.UseViteDevelopmentServer(/* false */);
 }
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    //app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
-    var webRootProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "wwwroot"));
-    var distProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "dist"));
-    var compositeProvider = new CompositeFileProvider(webRootProvider, distProvider);
-    app.Environment.WebRootFileProvider = compositeProvider;
-    app.Environment.WebRootPath = distProvider.Root;
-}
 
 app.UseCors("AllowReactApp");
 
